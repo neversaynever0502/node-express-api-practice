5d856d696d7fb018099c60bd3a087f6f0e0cfba1
Kai-Lin <kai-lin@Kai-Linde-MacBook-Pro.local>
2017-02-17 20:56:43 +0800

    better

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.all('*', function(req, res, next) {  
     res.header("Access-Control-Allow-Origin", "*");  
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");  
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, X-Requested-With");  
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
     res.header("X-Powered-By",' 3.2.1');  
     res.header("Content-Type", "application/json;charset=utf-8");  
