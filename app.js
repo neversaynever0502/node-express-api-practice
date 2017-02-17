d274a4cc787236e3287460c00d7839319d11bb06
Kai-Lin <kai-lin@Kai-Linde-MacBook-Pro.local>
2017-02-17 15:58:29 +0800

    better

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use('/users', users);
 // app.use('/bot',bots);
 
 var bot = linebot({
-  channelId: 1501711063,
+  channelId: "1501711063",
   channelSecret: "8f0a3770e17a34091ae10d23d60f1e8b",
   channelAccessToken: "FwZsKqF19GuRSqB/IS04XpYZ13IVXd+uKQjFG3L0J46gggw096I8fTXGm7OR2FxeWu9qu4wNXSAyoT8GoWUz1gp4xrum/czwT7RL5SSwqk2kiSipawNmBEOyE00eHNPo9FXNbUNvYQsVJ64p1VJbqgdB04t89/1O/w1cDnyilFU="
 });
