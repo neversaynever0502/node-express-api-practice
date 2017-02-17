e6ead38967542975183db1944d11c893ccf0fe20
Kai-Lin <kai-lin@Kai-Linde-MacBook-Pro.local>
2017-02-17 15:55:40 +0800

    better

--- routes/bots.js
+++ routes/bots.js
@@ -1,13 +1,10 @@
-var express = require('express');
-var router = express.Router();
-var robot = require('../func/bot');
+// var express = require('express');
+// var router = express.Router();
+// var robot = require('../func/bot');
 
-router.route('/').post(function(req, res) {
-    robot()
-  });
+// router.route('/').post(function(req, res) {
+//     robot()
+//   });
 
 
-// const app2 = express();
-// const linebotParser = bot.parser();
-// app2.post('/bot', linebotParser);
-module.exports = router;
+// module.exports = router;
