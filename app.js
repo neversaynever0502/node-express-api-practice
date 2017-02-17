5f9fdae81d678843f2da9d685356dd79fbbf8c60
Kai-Lin <kai-lin@Kai-Linde-MacBook-Pro.local>
2017-02-17 15:55:00 +0800

    better

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var linebot = require('linebot');
+
 
 var members = require('./routes/members'); //routes are defined here
 var index = require('./routes/index');
@@ -45,10 +47,8 @@ app.use('/', index.memberAll);
 app.use('/users', users);
 // app.use('/bot',bots);
 
-var linebot = require('linebot');
-
 var bot = linebot({
-  channelId: "1501711063",
+  channelId: 1501711063,
   channelSecret: "8f0a3770e17a34091ae10d23d60f1e8b",
   channelAccessToken: "FwZsKqF19GuRSqB/IS04XpYZ13IVXd+uKQjFG3L0J46gggw096I8fTXGm7OR2FxeWu9qu4wNXSAyoT8GoWUz1gp4xrum/czwT7RL5SSwqk2kiSipawNmBEOyE00eHNPo9FXNbUNvYQsVJ64p1VJbqgdB04t89/1O/w1cDnyilFU="
 });
